2c52b6780ac08de4460f448f752dddfbd7dec4ae
Brian P King <brian@jenashcal.net>
2021-05-17 19:39:53 -0700

    Restructuring.

--- cipher/railfence.go
+++ cipher/railfence.go
@@ -1,4 +1,4 @@
-package main
+package cipher
 
 import (
     "crypto/rand"
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+    "github.com/mutaboss/encrypt/util"
+    "github.com/mutaboss/encrypt/cipher"
 )
 
 // ************************************************************************************************
@@ -10,7 +12,7 @@ import (
 // ************************************************************************************************
 
 func main() {
-	terminal := NewTerminal(os.Stdin, os.Stdout)
+	terminal := util.NewTerminal(os.Stdin, os.Stdout)
 	for {
 		terminal.Prompt()
 		line := terminal.ReadLine()
@@ -18,7 +20,7 @@ func main() {
 			break
 		}
 		plaintext := line[:len(line)-1] // remove terminating newline
-		ciphertext := RailFenceCipherEncrypt(plaintext)
-		fmt.Printf("%s => %s => %s\n", plaintext, ciphertext, RailFenceCipherDecrypt(ciphertext))
+		ciphertext := cipher.RailFenceCipherEncrypt(plaintext)
+		fmt.Printf("%s => %s => %s\n", plaintext, ciphertext, cipher.RailFenceCipherDecrypt(ciphertext))
 	}
 }
